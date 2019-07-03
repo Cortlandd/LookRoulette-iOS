559fbee808d14634f12cc42b23ad4cd2ea2ffb20
Cortland Walker <cortlandjwalker@icloud.com>
2019-07-02 21:58:40 -0500

    Implement filters.

---
+++ Look Roulette/FilterCell.swift
@@ -0,0 +1,27 @@
+//
+//  FilterCell.swift
+//  Look Roulette
+//
+//  Created by Cortland Walker on 7/2/19.
+//  Copyright © 2019 Cortland Walker. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var _filterText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Look Roulette/FiltersViewController.swift
+++ Look Roulette/FiltersViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FiltersViewController: UIViewController {
+    
+    // The list of filters inside the Filters Table
+    var filters = [String]()
+    
+    // String representation of the filters created in the tableview. Separated with a space.
+    var allFiltersStringText: String = ""
+    
+    @IBOutlet weak var _addFilterText: UITextField!
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -16,32 +24,73 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func addFilterButton(_ sender: Any) {
+        insertFilter()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
     }
     
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     * Function used to append a filter to table view.
+     */
+    func insertFilter() {
+        
+        if _addFilterText.text == "" {
+            print("Add Video Text Field is empty")
+        } else {
+            filters.append(_addFilterText.text!)
+            
+            let indexPath = IndexPath(row: filters.count - 1, section: 0)
+            
+            tableView.beginUpdates()
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+            
+            _addFilterText.text = ""
+            view.endEditing(true)
+            
+            tableView.isHidden = false
+        }
     }
-    */
 
+}
+
+extension FiltersViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let filterTitle = filters[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FilterCell") as! FilterCell
+        cell._filterText.text = filterTitle
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            filters.remove(at: indexPath.row)
+            
+            tableView.beginUpdates()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+        }
+        
+    }
+    
+    
 }
